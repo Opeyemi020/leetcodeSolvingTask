330a260bf15f6ea4e79780b3664d5112eb995d0e
Opeyemi020 <laycon122@gmail.com>
2023-08-22 00:58:06 +0100

    first commit

---
+++ src/leetCodeTask/RepeatedSubStringPattern.java
@@ -0,0 +1,6 @@
+package leetCodeTask;
+
+public class RepeatedSubStringPattern {
+    
+    
+}
---
+++ test/leetCodeTask/RepeatedSubStringPatternTest.java
@@ -0,0 +1,10 @@
+package leetCodeTask;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatedSubStringPatternTest {
+
+    @Test
+    
+
+}
